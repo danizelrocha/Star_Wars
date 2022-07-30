745224155d02e439e70922a49ed128615ff01866
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2022-07-30 13:49:36 -0300

    primeiraAtualizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './page/home/home.component';
+import { PictureComponent } from './page/picture/picture.component';
+import { VideosComponent } from './page/videos/videos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    PictureComponent,
+    VideosComponent
   ],
   imports: [
     BrowserModule,
